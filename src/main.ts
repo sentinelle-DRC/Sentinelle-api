cd7729f6c6f862780674c6edbacaab88db6169e9
sirMelka <melkabotale1@gmail.com>
2023-04-23 23:45:24 +0200

    fix: enable Cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,9 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
