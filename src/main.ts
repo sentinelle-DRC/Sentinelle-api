56beeb6a18a1f2108ddfdfdb2a99b2b82ca03ed0
sirMelka <melkabotale1@gmail.com>
2023-04-24 21:41:25 +0200

    last test

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,22 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  var whitelist = [
+    'https://sentinelledrc.vercel.app',
+    'https://sentinelledrc.vercel.app/auth',
+    'https://sentinelledrc.vercel.app/home',
+    'sentinelledrc.vercel.app',
+    'sentinelledrc.vercel.app/auth',
+    'sentinelledrc.vercel.app/home',
+  ];
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
   });
   const config = new DocumentBuilder()
     .setTitle('Sentinelle api')
