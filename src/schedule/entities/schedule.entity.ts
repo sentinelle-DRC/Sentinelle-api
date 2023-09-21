ded41dd3e6d31644ce82978a0cf75acb065aa7b3
sirMelka <melkabotale1@gmai.com>
2023-09-21 13:34:49 +0100

    feat: create schedule entity

--- src/schedule/entities/schedule.entity.ts
+++ src/schedule/entities/schedule.entity.ts
@@ -1,1 +1,29 @@
-export class Schedule {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Type } from "class-transformer";
+import mongoose from "mongoose";
+import { Class } from "src/class/entities/class.entity";
+import { Course } from "src/course/entities/course.entity";
+
+export type ScheduleDocument = Schedule & Document;
+
+@Schema()
+export class Schedule {
+    @Prop({required:true})
+    day:String
+
+    @Prop({required:true})
+    startHour:Date
+
+    @Prop({required:true})
+    endHour:Date
+
+    @Prop({required:true,type:mongoose.Schema.Types.ObjectId,ref:Course.name})
+    @Type(()=>Course)
+    course = mongoose.Schema.Types.ObjectId
+
+    @Prop({required:true,type:mongoose.Schema.Types.ObjectId,ref:Class.name})
+    @Type(()=>Class)
+    class = mongoose.Schema.Types.ObjectId
+}
+ 
+export const ScheduleSchema = SchemaFactory.createForClass(Schedule)
